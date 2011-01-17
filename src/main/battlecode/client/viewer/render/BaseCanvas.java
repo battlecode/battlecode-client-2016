a788db23635c760a1ff1a1457ceba41ddd7f6b6b
aibolit <aibolit@mit.edu>
2011-01-17 12:49:12 -0500

    meh whatever netbeans was complaining

--- src/main/battlecode/client/viewer/render/BaseCanvas.java
+++ src/main/battlecode/client/viewer/render/BaseCanvas.java
@@ -2,87 +2,99 @@ package battlecode.client.viewer.render;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.HierarchyEvent;
 import java.awt.event.HierarchyListener;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
 import java.util.Observer;
 
 import javax.swing.*;
 
 public abstract class BaseCanvas extends JPanel {
 
-	protected Runnable spaceBarListener = null;
+    protected Runnable spaceBarListener = null;
 
-	public BaseCanvas() {
-		super();
+    public BaseCanvas() {
+        super();
 
-		setVisible(false);
-		addHierarchyListener(new HierarchyListener() {
-			public void hierarchyChanged(HierarchyEvent e) {
-				if (isDisplayable()) {
-					initKeyBindings();
-					removeHierarchyListener(this);
-				}
-			}
-		});
-		addComponentListener(new ComponentAdapter() {
-			public void componentResized(ComponentEvent e) {
-				if (getRenderer() != null) { getRenderer().setCanvasSize(getSize()); }
-			}
-		});
-		setAlignmentX(CENTER_ALIGNMENT);
-		setBackground(Color.BLACK);
-		setMaximumSize(new Dimension(Short.MAX_VALUE, Short.MAX_VALUE));
-		setPreferredSize(new Dimension(800, 600));
-		setOpaque(true);
+        setVisible(false);
+        addHierarchyListener(new HierarchyListener() {
 
-	}
+            public void hierarchyChanged(HierarchyEvent e) {
+                if (isDisplayable()) {
+                    initKeyBindings();
+                    removeHierarchyListener(this);
+                }
+            }
+        });
+        addComponentListener(new ComponentAdapter() {
 
-	protected void initKeyBindings() {
-		InputMap im = getInputMap(WHEN_IN_FOCUSED_WINDOW);
-		im.put(KeyStroke.getKeyStroke("ESCAPE"), "exit");
-		for (char c = 'A'; c<='Z'; c++) {
-			im.put(KeyStroke.getKeyStroke(String.valueOf(c)), "toggle");
-		}
-		if (RenderConfiguration.isTournamentMode()) {
-			im.put(KeyStroke.getKeyStroke("SPACE"), "space");
-			//im.put(KeyStroke.getKeyStroke("S"), "toggle");
-		}
-		getActionMap().put("toggle", new AbstractAction() {
-			private static final long serialVersionUID = 0; // don't serialize
-			public void actionPerformed(ActionEvent e) {
-				if (getRenderer() != null) {
-					getRenderer().handleAction(e.getActionCommand().charAt(0));
-					repaint();
-				}
-			}
-		});
-		getActionMap().put("space", new AbstractAction() {
-			private static final long serialVersionUID = 0; // don't serialize
-			public void actionPerformed(ActionEvent e) {
-				if (spaceBarListener != null) { spaceBarListener.run(); }
-			}
-		});
-		getActionMap().put("exit", new AbstractAction() {
-			private static final long serialVersionUID = 0; // don't serialize
-			public void actionPerformed(ActionEvent e) { System.exit(0); }
-		});
-	}
+            @Override
+            public void componentResized(ComponentEvent e) {
+                if (getRenderer() != null) {
+                    getRenderer().setCanvasSize(getSize());
+                }
+            }
+        });
+        setAlignmentX(CENTER_ALIGNMENT);
+        setBackground(Color.BLACK);
+        setMaximumSize(new Dimension(Short.MAX_VALUE, Short.MAX_VALUE));
+        setPreferredSize(new Dimension(800, 600));
+        setOpaque(true);
 
-	protected abstract BaseRenderer getRenderer();
+    }
 
-	public void setSpaceBarListener(Runnable runnable) {
-		spaceBarListener = runnable;
-	}
+    protected void initKeyBindings() {
+        InputMap im = getInputMap(WHEN_IN_FOCUSED_WINDOW);
+        im.put(KeyStroke.getKeyStroke("ESCAPE"), "exit");
+        for (char c = 'A'; c <= 'Z'; c++) {
+            im.put(KeyStroke.getKeyStroke(String.valueOf(c)), "toggle");
+        }
+        if (RenderConfiguration.isTournamentMode()) {
+            im.put(KeyStroke.getKeyStroke("SPACE"), "space");
+            //im.put(KeyStroke.getKeyStroke("S"), "toggle");
+        }
+        getActionMap().put("toggle", new AbstractAction() {
 
-	public abstract void setRenderer(BaseRenderer renderer);
-	
-	public abstract void setTournamentMode();
+            private static final long serialVersionUID = 0; // don't serialize
 
-	public abstract void addPaintObserver(Observer o);
+            public void actionPerformed(ActionEvent e) {
+                if (getRenderer() != null) {
+                    getRenderer().handleAction(e.getActionCommand().charAt(0));
+                    repaint();
+                }
+            }
+        });
+        getActionMap().put("space", new AbstractAction() {
+
+            private static final long serialVersionUID = 0; // don't serialize
+
+            public void actionPerformed(ActionEvent e) {
+                if (spaceBarListener != null) {
+                    spaceBarListener.run();
+                }
+            }
+        });
+        getActionMap().put("exit", new AbstractAction() {
+
+            private static final long serialVersionUID = 0; // don't serialize
+
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    protected abstract BaseRenderer getRenderer();
+
+    public void setSpaceBarListener(Runnable runnable) {
+        spaceBarListener = runnable;
+    }
+
+    public abstract void setRenderer(BaseRenderer renderer);
+
+    public abstract void setTournamentMode();
+
+    public abstract void addPaintObserver(Observer o);
 }
--- src/main/battlecode/client/viewer/render/GameCanvas.java
+++ src/main/battlecode/client/viewer/render/GameCanvas.java
@@ -5,87 +5,77 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
-import java.awt.event.ActionEvent;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
-import java.awt.event.HierarchyEvent;
-import java.awt.event.HierarchyListener;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
 import java.util.Observable;
 import java.util.Observer;
 
-import javax.swing.AbstractAction;
-import javax.swing.InputMap;
-import javax.swing.KeyStroke;
 
 import battlecode.client.util.ImageFile;
 import battlecode.client.viewer.renderer3d.GLGameRenderer;
 
 public final class GameCanvas extends BaseCanvas {
 
-	private static final long serialVersionUID = 0; // don't serialize
+    private static final long serialVersionUID = 0; // don't serialize
+    private volatile GameRenderer renderer = null;
+    private Runnable spaceBarListener = null;
+    private boolean gotPreferredSize = false;
 
-	private volatile GameRenderer renderer = null;
-	private Runnable spaceBarListener = null;
+    //private Window fullscreen;
+    public GameCanvas() {
+        super();
+    }
 
-	private boolean gotPreferredSize = false;
+    public void setTournamentMode() {
+        RenderConfiguration.setTournamentMode(true);
+        setVisible(true);
+    }
 
-	//private Window fullscreen;
-	
-	public GameCanvas() {
-		super();
-	}
+    protected BaseRenderer getRenderer() {
+        return renderer;
+    }
 
-	public void setTournamentMode() {
-		RenderConfiguration.setTournamentMode(true);
-		setVisible(true);
-	}
+    public void setRenderer(BaseRenderer r) {
+        renderer = (GameRenderer) r;
+        /*
+        for (MouseListener ml: getMouseListeners()) {
+        removeMouseListener(ml);
+        }
+        for (MouseMotionListener mml: getMouseMotionListeners()) {
+        removeMouseMotionListener(mml);
+        }
+         */
+        //setVisible(true);
 
-	protected BaseRenderer getRenderer() { return renderer; }
+        Observer observer = new Observer() {
 
-	public void setRenderer(BaseRenderer r) {
-		renderer = (GameRenderer)r;
-		/*
-		for (MouseListener ml: getMouseListeners()) {
-			removeMouseListener(ml);
-		}
-		for (MouseMotionListener mml: getMouseMotionListeners()) {
-			removeMouseMotionListener(mml);
-		}
-		*/
-		//setVisible(true);
+            public void update(Observable o, Object arg) {
+                if (renderer.getTimeline().isActive()) {
+                    forceRepaint();
+                }
+            }
+        };
+        renderer.getTimeline().addObserver(observer);
 
-		Observer observer = new Observer() {
-				public void update(Observable o, Object arg) {
-					if(renderer.getTimeline().isActive()) {
-						forceRepaint();
-					}
-				}
-			};
-		renderer.getTimeline().addObserver(observer);
+        battlecode.client.viewer.DebugState dbg = renderer.getDebugState();
+        addMouseListener(dbg);
+        addMouseMotionListener(dbg);
+        dbg.addObserver(observer);
 
-		battlecode.client.viewer.DebugState dbg = renderer.getDebugState();
-		addMouseListener(dbg);
-		addMouseMotionListener(dbg);
-		dbg.addObserver(observer);
+        repaint();
+    }
+    public static ImageFile bracketFile = null;
+    public final BaseRenderer bracketRenderer = new GameRenderer() {
 
-		repaint();
-	}
-	
-	public static ImageFile bracketFile = null;
-	public final BaseRenderer bracketRenderer = new GameRenderer() {
-		public void draw(Graphics g) {
-			if (RenderConfiguration.isTournamentMode()) {
-				Graphics2D g2 = (Graphics2D) g;
-				Dimension d = getSize();
-				g2.setColor(Color.GRAY);
-				g2.fill(new Rectangle(0, 0, d.width, d.height));
-				
-				
+        @Override
+        public void draw(Graphics g) {
+            if (RenderConfiguration.isTournamentMode()) {
+                Graphics2D g2 = (Graphics2D) g;
+                Dimension d = getSize();
+                g2.setColor(Color.GRAY);
+                g2.fill(new Rectangle(0, 0, d.width, d.height));
+
+
 //				System.out.println("Invoking python script to move pngs around");
 //				
 //				Process c = null;
@@ -102,52 +92,50 @@ public final class GameCanvas extends BaseCanvas {
 //					// TODO Auto-generated catch block
 //					e.printStackTrace();
 //				}
-				
-				if (bracketFile == null) {
-					bracketFile = new ImageFile(String.format("%d.png", GLGameRenderer.BRACKET_INDEX));
-				}
-				BufferedImage img = bracketFile.image;
-				if (img != null) {
-					float scale = Math.min((float) d.width / img.getWidth(),
-					                       (float) d.height / img.getHeight());
-					AffineTransform trans = AffineTransform.getTranslateInstance(0.5*(d.width - scale*img.getWidth()), 0.5*(d.height - scale*img.getHeight()));
-					trans.scale(scale, scale);
-					g2.drawImage(img, trans, null);
-				}
-			}
-		}
-	};
 
-	private final battlecode.client.util.SettableObservable paintObservable = new battlecode.client.util.SettableObservable();
+                if (bracketFile == null) {
+                    bracketFile = new ImageFile(String.format("%d.png", GLGameRenderer.BRACKET_INDEX));
+                }
+                BufferedImage img = bracketFile.image;
+                if (img != null) {
+                    float scale = Math.min((float) d.width / img.getWidth(),
+                            (float) d.height / img.getHeight());
+                    AffineTransform trans = AffineTransform.getTranslateInstance(0.5 * (d.width - scale * img.getWidth()), 0.5 * (d.height - scale * img.getHeight()));
+                    trans.scale(scale, scale);
+                    g2.drawImage(img, trans, null);
+                }
+            }
+        }
+    };
+    private final battlecode.client.util.SettableObservable paintObservable = new battlecode.client.util.SettableObservable();
 
-	public void addPaintObserver(Observer o) {
-		paintObservable.addObserver(o);
-	}
+    public void addPaintObserver(Observer o) {
+        paintObservable.addObserver(o);
+    }
 
-	public void paint(Graphics g) {
-		if (renderer != null) {
-			renderer.setCanvasSize(getSize());
-			renderer.draw(g);
-			paintObservable.setChanged();
-			paintObservable.notifyObservers();
-		}
-		else {
-			g.setColor(Color.BLACK);
-			Dimension d = getSize();
-			g.fillRect(0, 0, d.width, d.height);
-		}
-	}
+    @Override
+    public void paint(Graphics g) {
+        if (renderer != null) {
+            renderer.setCanvasSize(getSize());
+            renderer.draw(g);
+            paintObservable.setChanged();
+            paintObservable.notifyObservers();
+        } else {
+            g.setColor(Color.BLACK);
+            Dimension d = getSize();
+            g.fillRect(0, 0, d.width, d.height);
+        }
+    }
 
-	public void forceRepaint() {
-		if(!gotPreferredSize) {
-			Dimension preferredSize = renderer.getPreferredSize();
-			if(preferredSize.getHeight()!=0&&preferredSize.getWidth()!=0) {
-				setPreferredSize(preferredSize);
-				gotPreferredSize=true;
-				setVisible(true);
-			}
-		}
-		repaint();
-	}
-
+    public void forceRepaint() {
+        if (!gotPreferredSize) {
+            Dimension preferredSize = renderer.getPreferredSize();
+            if (preferredSize.getHeight() != 0 && preferredSize.getWidth() != 0) {
+                setPreferredSize(preferredSize);
+                gotPreferredSize = true;
+                setVisible(true);
+            }
+        }
+        repaint();
+    }
 }
