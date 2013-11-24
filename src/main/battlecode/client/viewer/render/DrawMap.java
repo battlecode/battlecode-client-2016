85450220f4f9af1ebf45c3609ee9aa2e9c2f6650
Aaron Epstein <aepstein824@gmail.com>
2013-11-24 03:49:38 -0500

    circle based visualization

--- src/main/battlecode/client/viewer/render/DrawMap.java
+++ src/main/battlecode/client/viewer/render/DrawMap.java
@@ -27,111 +27,111 @@ import static battlecode.common.TerrainTile.*;
 
 public class DrawMap {
 
-    private int mapWidth;
-    private int mapHeight;
-    private BufferedImage prerender;
-    private Stroke gridStroke;
-    private float scaleSize;
-    // number of blocks at each square
-    // origin of the map
-    MapLocation origin;
-    private int imgSize = 32;
-    public battlecode.world.GameMap m;
+  private int mapWidth;
+  private int mapHeight;
+  private BufferedImage prerender;
+  private Stroke gridStroke;
+  private float scaleSize;
+  // number of blocks at each square
+  // origin of the map
+  MapLocation origin;
+  private int imgSize = 32;
+  public battlecode.world.GameMap m;
 
-    public DrawMap(battlecode.world.GameMap map) {
-        mapWidth = map.getWidth();
-        mapHeight = map.getHeight();
-        origin = map.getMapOrigin();
+  public DrawMap(battlecode.world.GameMap map) {
+    mapWidth = map.getWidth();
+    mapHeight = map.getHeight();
+    origin = map.getMapOrigin();
 
-        //FIXME: commented out for now
-//		if (!RenderConfiguration.getInstance().isTournamentMode()) {
-        prerenderMap(map);
-//		}
-        gridStroke = new BasicStroke(0.1f / RenderConfiguration.getInstance().getSpriteSize());
+    //FIXME: commented out for now
+//    if (!RenderConfiguration.getInstance().isTournamentMode()) {
+    prerenderMap(map);
+//    }
+    gridStroke = new BasicStroke(0.1f / RenderConfiguration.getInstance().getSpriteSize());
 
+  }
+
+  public void redraw() {
+    prerenderMap(this.m);
+  }
+
+  public void prerenderMap(battlecode.world.GameMap m) {
+    this.m = m;
+    TerrainTile[][] map = m.getTerrainMatrix();
+
+    byte[][] indices = new byte[mapWidth + 1][mapHeight + 1]; // init indices
+    for (int j = 0; j <= mapHeight; j++) for (int i = 0; i <= mapWidth; i++) {
+        int top = (j == 0 ? 0x03 : (indices[i][j - 1] & 0x03));
+        int bottom = (j == mapHeight ? 0x03 : (i == 0 || map[i - 1][j] == VOID ? 0x02 : 0x00)
+                      | (i == mapWidth || map[i][j] == VOID ? 0x01 : 0x00));
+        indices[i][j] = (byte) (top << 2 | bottom);
+      }
+    ImageFile terrainImg = new ImageFile("art/terrain.png"); // actual rendering
+    BufferedImage image = terrainImg.image;
+    assert image.getWidth() == image.getHeight();
+    imgSize = image.getWidth() / 4;
+    BufferedImage[] tiles = new BufferedImage[16];
+    byte[][] imgToMap = {
+      {8, 1, 7, 14},
+      {0, 5, 15, 10},
+      {2, 4, 13, 11},
+      {9, 3, 6, 12}};
+    for (int row = 0; row < 4; row++) for (int col = 0; col < 4; col++) {
+        tiles[imgToMap[row][col]] =
+          image.getSubimage(col * imgSize, row * imgSize, imgSize, imgSize);
+      }
+    prerender = RenderConfiguration.createCompatibleImage(imgSize * mapWidth,
+                                                          imgSize * mapHeight);
+
+    Graphics2D g2 = prerender.createGraphics();
+    for (int i = 0; i < mapWidth; i++) {
+      for (int j = 0; j < mapHeight; j++) {
+        byte index = indices[i][j];
+        assert 0 <= index && index < 16;
+        g2.drawImage(tiles[index], null, imgSize * i - imgSize / 2, imgSize * j - imgSize / 2);
+      }
     }
 
-    public void redraw() {
-        prerenderMap(this.m);
-    }
+    g2.dispose();
+    terrainImg.unload();
 
-    public void prerenderMap(battlecode.world.GameMap m) {
-        this.m = m;
-        TerrainTile[][] map = m.getTerrainMatrix();
+    scaleSize = imgSize;
+  }
 
-        byte[][] indices = new byte[mapWidth + 1][mapHeight + 1]; // init indices
-        for (int j = 0; j <= mapHeight; j++) for (int i = 0; i <= mapWidth; i++) {
-                int top = (j == 0 ? 0x03 : (indices[i][j - 1] & 0x03));
-                int bottom = (j == mapHeight ? 0x03 : (i == 0 || map[i - 1][j] == VOID ? 0x02 : 0x00)
-                        | (i == mapWidth || map[i][j] == VOID ? 0x01 : 0x00));
-                indices[i][j] = (byte) (top << 2 | bottom);
-            }
-        ImageFile terrainImg = new ImageFile("art/terrain.png"); // actual rendering
-        BufferedImage image = terrainImg.image;
-        assert image.getWidth() == image.getHeight();
-        imgSize = image.getWidth() / 4;
-        BufferedImage[] tiles = new BufferedImage[16];
-        byte[][] imgToMap = {
-            {8, 1, 7, 14},
-            {0, 5, 15, 10},
-            {2, 4, 13, 11},
-            {9, 3, 6, 12}};
-        for (int row = 0; row < 4; row++) for (int col = 0; col < 4; col++) {
-                tiles[imgToMap[row][col]] =
-                        image.getSubimage(col * imgSize, row * imgSize, imgSize, imgSize);
-            }
-        prerender = RenderConfiguration.createCompatibleImage(imgSize * mapWidth,
-                imgSize * mapHeight);
+  public void prerenderMap(BufferedImage bg) {
+    scaleSize = (float) bg.getWidth() / mapWidth;
+    prerender = bg;
+  }
 
-        Graphics2D g2 = prerender.createGraphics();
-        for (int i = 0; i < mapWidth; i++) {
-			for (int j = 0; j < mapHeight; j++) {
-				byte index = indices[i][j];
-				assert 0 <= index && index < 16;
-				g2.drawImage(tiles[index], null, imgSize * i - imgSize / 2, imgSize * j - imgSize / 2);
-            }
-		}
+  public int getMapWidth() {
+    return mapWidth;
+  }
 
-        g2.dispose();
-        terrainImg.unload();
+  public int getMapHeight() {
+    return mapHeight;
+  }
 
-        scaleSize = imgSize;
-    }
+  public void draw(Graphics2D g2, DrawState ds) {
+    AffineTransform pushed = g2.getTransform();
+    g2.scale(1.0 / scaleSize, 1.0 / scaleSize);
 
-    public void prerenderMap(BufferedImage bg) {
-        scaleSize = (float) bg.getWidth() / mapWidth;
-        prerender = bg;
-    }
-
-    public int getMapWidth() {
-        return mapWidth;
-    }
-
-    public int getMapHeight() {
-        return mapHeight;
-    }
-
-    public void draw(Graphics2D g2, DrawState ds) {
-        AffineTransform pushed = g2.getTransform();
-        g2.scale(1.0 / scaleSize, 1.0 / scaleSize);
-
-        g2.drawImage(prerender, null, null);
+    g2.drawImage(prerender, null, null);
         
-		g2.setTransform(pushed);
-        if (RenderConfiguration.showGridlines()) {
-            g2.setColor(new Color(0.4f, 0.4f, 0.4f, 1.0f));
-            g2.setStroke(gridStroke);
-            Line2D.Float gridline = new Line2D.Float(0, 0, 0, mapHeight);
-            for (int i = 1; i < mapWidth; i += 1) {
-                gridline.x1 = gridline.x2 = i;
-                g2.draw(gridline);
-            }
-            gridline.x1 = 0;
-            gridline.x2 = mapWidth;
-            for (int i = 1; i < mapHeight; i += 1) {
-                gridline.y1 = gridline.y2 = i;
-                g2.draw(gridline);
-            }
-        }
+    g2.setTransform(pushed);
+    if (RenderConfiguration.showGridlines()) {
+      g2.setColor(new Color(0.4f, 0.4f, 0.4f, 1.0f));
+      g2.setStroke(gridStroke);
+      Line2D.Float gridline = new Line2D.Float(0, 0, 0, mapHeight);
+      for (int i = 1; i < mapWidth; i += 1) {
+        gridline.x1 = gridline.x2 = i;
+        g2.draw(gridline);
+      }
+      gridline.x1 = 0;
+      gridline.x2 = mapWidth;
+      for (int i = 1; i < mapHeight; i += 1) {
+        gridline.y1 = gridline.y2 = i;
+        g2.draw(gridline);
+      }
     }
+  }
 }
--- src/main/battlecode/client/viewer/render/DrawState.java
+++ src/main/battlecode/client/viewer/render/DrawState.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
+import java.awt.geom.Ellipse2D;
 import java.awt.image.BufferedImage;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
@@ -33,139 +34,139 @@ public class DrawState extends AbstractDrawState<DrawObject> {
 	private static final ImageFile rNukeb2 = new ImageFile("art/nukeb2.png");
 	private static final ImageFile rexplode = new ImageFile("art/nukeexplode.png");
 	
-    protected static final Color dragShadow = new Color(0.5f, 0.5f, 0.5f, 0.5f);
+  protected static final Color dragShadow = new Color(0.5f, 0.5f, 0.5f, 0.5f);
 	protected static final Color linkNone = new Color(0.f,0.f,0.f);
 	protected static final Color linkA = new Color(1.f,0.f,0.f);
 	protected static final Color linkB = new Color(0.f,0.f,1.f);
 	protected static final Color linkBoth = new Color(.75f,0.f,.75f);
 	protected static final ImageFile encampment = new ImageFile("art/encampment.png");
 
-    private static class Factory implements GameStateFactory<DrawState> {
+  private static class Factory implements GameStateFactory<DrawState> {
 
-        public DrawState createState(GameMap map) {
-            return new DrawState(map);
-        }
-
-        public DrawState cloneState(DrawState state) {
-            return new DrawState(state);
-        }
-
-        public void copyState(DrawState src, DrawState dst) {
-            dst.copyStateFrom(src);
-        }
-    }
-    public static final GameStateFactory<DrawState> FACTORY = new Factory();
-    //private Map<Integer, DrawObject> groundUnits;
-    //private Map<Integer, DrawObject> airUnits;
-    private List<DrawObject> towers;
-    // these need to be drawn before all the units,
-    // so don't draw them from DrawObjects
-    //private ArrayList<TeleportAnim> teleportAnims;
-    private MapLocation[][] convexHullsA, convexHullsB;
-    // number of blocks in current draw state
-    //int[][] blockNumber;
-
-    public DrawState() {
-        groundUnits = new LinkedHashMap<Integer, DrawObject>();
-        airUnits = new LinkedHashMap<Integer, DrawObject>();
-        encampments = new HashSet<MapLocation>();
-        towers = new LinkedList<DrawObject>();
-        fluxDeposits = new LinkedHashMap<Integer, FluxDepositState>();
-        currentRound = -1;
-        convexHullsA = new MapLocation[0][];
-        convexHullsB = new MapLocation[0][];
-
+    public DrawState createState(GameMap map) {
+      return new DrawState(map);
     }
 
-    private DrawState(GameMap map) {
-        this();
-        this.setGameMap(map);
+    public DrawState cloneState(DrawState state) {
+      return new DrawState(state);
     }
 
-    private DrawState(DrawState clone) {
-        this();
-        copyStateFrom(clone);
+    public void copyState(DrawState src, DrawState dst) {
+      dst.copyStateFrom(src);
     }
+  }
+  public static final GameStateFactory<DrawState> FACTORY = new Factory();
+  //private Map<Integer, DrawObject> groundUnits;
+  //private Map<Integer, DrawObject> airUnits;
+  private List<DrawObject> towers;
+  // these need to be drawn before all the units,
+  // so don't draw them from DrawObjects
+  //private ArrayList<TeleportAnim> teleportAnims;
+  private MapLocation[][] convexHullsA, convexHullsB;
+  // number of blocks in current draw state
+  //int[][] blockNumber;
 
-    protected DrawObject createDrawObject(RobotType type, Team team, int id) {
-        return new DrawObject(type, team, id, this);
-    }
+  public DrawState() {
+    groundUnits = new LinkedHashMap<Integer, DrawObject>();
+    airUnits = new LinkedHashMap<Integer, DrawObject>();
+    encampments = new HashSet<MapLocation>();
+    towers = new LinkedList<DrawObject>();
+    fluxDeposits = new LinkedHashMap<Integer, FluxDepositState>();
+    currentRound = -1;
+    convexHullsA = new MapLocation[0][];
+    convexHullsB = new MapLocation[0][];
+
+  }
+
+  private DrawState(GameMap map) {
+    this();
+    this.setGameMap(map);
+  }
+
+  private DrawState(DrawState clone) {
+    this();
+    copyStateFrom(clone);
+  }
+
+  protected DrawObject createDrawObject(RobotType type, Team team, int id) {
+    return new DrawObject(type, team, id, this);
+  }
 
 	protected DrawObject createDrawObject(DrawObject o) {
 		return new DrawObject(o);
 	}
 
-    public MapLocation[][] getConvexHullsA() {
-        return convexHullsA;
-    }
+  public MapLocation[][] getConvexHullsA() {
+    return convexHullsA;
+  }
 
-    public MapLocation[][] getConvexHullsB() {
-        return convexHullsB;
-    }
+  public MapLocation[][] getConvexHullsB() {
+    return convexHullsB;
+  }
     
-    public double getTeamResources(Team t) {
-    	return teamResources[t.ordinal()];
+  public double getTeamResources(Team t) {
+    return teamResources[t.ordinal()];
+  }
+
+  public double getResearchProgress(Team t, int i) {
+    return researchProgress[t.ordinal()][i];
+  }
+
+  public synchronized void apply(RoundStats stats) {
+      this.stats = stats;
     }
 
-    public double getResearchProgress(Team t, int i) {
-    	return researchProgress[t.ordinal()][i];
+  public DrawObject getDrawObject(int id) {
+    try {
+      return getRobot(id);
+    } catch (AssertionError e) {
+      return null;
     }
+  }
 
-    public synchronized void apply(RoundStats stats) {
-        this.stats = stats;
-    }
+  private void drawDragged(Graphics2D g2, DebugState debug, DrawObject obj) {
+    /*
+      MapLocation loc = obj.getLocation();
+      float dx = debug.getDX(), dy = debug.getDY();
+      g2.setColor(dragShadow);
+      g2.fill(new Rectangle2D.Float(Math.round(loc.x + dx),
+      Math.round(loc.y + dy), 1, 1));
+      AffineTransform pushed = g2.getTransform(); // push
+      g2.translate(dx, dy);
+      obj.draw(g2, true);
+      g2.setTransform(pushed); // pop;
+    */
+  }
 
-    public DrawObject getDrawObject(int id) {
-        try {
-            return getRobot(id);
-        } catch (AssertionError e) {
-            return null;
-        }
-    }
-
-    private void drawDragged(Graphics2D g2, DebugState debug, DrawObject obj) {
+  /**
+   * Draws the current game state. This method is always called from the
+   * Swing event-dispatch thread, and in particular blocks calls to
+   * updateRound.
+   * @param g2 The graphics context, transformed to MapLocation-space
+   * @param debug The debug state, including MapLocation-space mouse state
+   */
+  public synchronized void draw(Graphics2D g2, DebugState debug) {
+      if (RenderConfiguration.showSpawnRadii()) {
         /*
-        MapLocation loc = obj.getLocation();
-        float dx = debug.getDX(), dy = debug.getDY();
-        g2.setColor(dragShadow);
-        g2.fill(new Rectangle2D.Float(Math.round(loc.x + dx),
-        Math.round(loc.y + dy), 1, 1));
-        AffineTransform pushed = g2.getTransform(); // push
-        g2.translate(dx, dy);
-        obj.draw(g2, true);
-        g2.setTransform(pushed); // pop;
-         */
-    }
+          for (DrawObject tower : towers) {
+          tower.drawSpawnRadius(g2);
+          }
+        */
+      }
+      int dragID = debug.getDragID();
+      int focusID = debug.getFocusID();
+      int hoverID = -1;
+      MapLocation hoverLoc = null;
+      long controlBits = 0;
+      Iterable<Map.Entry<Integer, DrawObject>> drawableSet = getDrawableSet();
 
-    /**
-     * Draws the current game state. This method is always called from the
-     * Swing event-dispatch thread, and in particular blocks calls to
-     * updateRound.
-     * @param g2 The graphics context, transformed to MapLocation-space
-     * @param debug The debug state, including MapLocation-space mouse state
-     */
-    public synchronized void draw(Graphics2D g2, DebugState debug) {
-        if (RenderConfiguration.showSpawnRadii()) {
-            /*
-            for (DrawObject tower : towers) {
-            tower.drawSpawnRadius(g2);
-            }
-             */
-        }
-        int dragID = debug.getDragID();
-        int focusID = debug.getFocusID();
-        int hoverID = -1;
-        MapLocation hoverLoc = null;
-        long controlBits = 0;
-        Iterable<Map.Entry<Integer, DrawObject>> drawableSet = getDrawableSet();
-
-        if (drawableSet == null) {
-            return;
-        }
+      if (drawableSet == null) {
+        return;
+      }
 
 //		AffineTransform pushed2 = g2.getTransform();
 		
-		// Woohoo the power grid!
+      // Woohoo the power grid!
 //		g2.setStroke(new BasicStroke(.15f));
 //		g2.translate(.5,.5);
 //		for (Link l : links) {
@@ -182,42 +183,46 @@ public class DrawState extends AbstractDrawState<DrawObject> {
 //			g2.drawLine(l.from.x,l.from.y,l.to.x,l.to.y);
 //		}
 
-        double maxDensity = 0.0;
-        for (int i = 0; i < neutralsDensity.length; i++) {
-            for (int j = 0; j < neutralsDensity.length; j++) {
-                maxDensity = Math.max(maxDensity, neutralsDensity[i][j]);
-            }
+      double maxDensity = 0.0;
+      for (int i = 0; i < neutralsDensity.length; i++) {
+        for (int j = 0; j < neutralsDensity.length; j++) {
+          maxDensity = Math.max(maxDensity, neutralsDensity[i][j]);
         }
-        for (int i = 0; i < neutralsDensity.length; i++) {
-            for (int j = 0; j < neutralsDensity.length; j++) {
-                int x = (int) (256 * Math.log(neutralsDensity[i][j]) / Math.log(maxDensity));
-                if (x < 0) x = 0;
-                if (x > 255) x = 255;
-                g2.setColor(new Color(x, x, x));
-                g2.fill(new Rectangle2D.Double((float) i, (float) j, 1.0f, 1.0f));
-            }
+      }
+
+      for (int i = 0; i < neutralsDensity.length; i++) {
+        for (int j = 0; j < neutralsDensity.length; j++) {
+          double density = (int)neutralsDensity[i][j];
+          // this is a logarithmic dot display for cow density
+          float greenChannel = (float)(.5 * density / maxDensity + .25);
+          g2.setColor(new Color(0.0f, greenChannel, 0.0f, 1.0f));
+          float maxPossible = 2000;
+          float size = (float) Math.sqrt(density / maxPossible);
+          float offset = ((1.0f - size) / 2);
+          g2.fill(new Ellipse2D.Float(i + offset, j + offset, size, size));
         }
+      }
 		
-		for (Entry<MapLocation, Team> entry : mineLocs.entrySet()) {
-			MapLocation loc = entry.getKey();
-			Team team = entry.getValue();
+      for (Entry<MapLocation, Team> entry : mineLocs.entrySet()) {
+        MapLocation loc = entry.getKey();
+        Team team = entry.getValue();
 		
-			if (team == Team.A) g2.setColor(new Color(1.f,0.f,0.f,.5f));
-			else if (team == Team.B) g2.setColor(new Color(0.f,0.f,1.f,.5f));
-			else g2.setColor(new Color(0.1f, 0.1f, 0.1f, 0.5f));
+        if (team == Team.A) g2.setColor(new Color(1.f,0.f,0.f,.5f));
+        else if (team == Team.B) g2.setColor(new Color(0.f,0.f,1.f,.5f));
+        else g2.setColor(new Color(0.1f, 0.1f, 0.1f, 0.5f));
 		
-			g2.fill(new Rectangle2D.Float(loc.x+0.1f, loc.y+0.1f, .9f, .9f));
-		}
+        g2.fill(new Rectangle2D.Float(loc.x+0.1f, loc.y+0.1f, .9f, .9f));
+      }
 	
 		
-		BufferedImage target = encampment.image;
-        for (MapLocation m : getEncampmentLocations()) {
-			AffineTransform trans = AffineTransform.getTranslateInstance(m.x, m.y);
-			trans.scale(1.0 / target.getWidth(), 1.0 / target.getHeight());
-			g2.drawImage(target, trans, null);
-			//g2.setColor(new Color(0.0f,0.0f,0.0f,1.0f));
-			//g2.fill(new Ellipse2D.Float(m.x, m.y, 1, 1));
-        }
+      BufferedImage target = encampment.image;
+      for (MapLocation m : getEncampmentLocations()) {
+        AffineTransform trans = AffineTransform.getTranslateInstance(m.x, m.y);
+        trans.scale(1.0 / target.getWidth(), 1.0 / target.getHeight());
+        g2.drawImage(target, trans, null);
+        //g2.setColor(new Color(0.0f,0.0f,0.0f,1.0f));
+        //g2.fill(new Ellipse2D.Float(m.x, m.y, 1, 1));
+      }
         
 		
 		
@@ -232,64 +237,64 @@ public class DrawState extends AbstractDrawState<DrawObject> {
 
 //		g2.setTransform(pushed2);
 
-        for (Map.Entry<Integer, DrawObject> entry : drawableSet) {
+      for (Map.Entry<Integer, DrawObject> entry : drawableSet) {
 
-            int id = entry.getKey();
-            DrawObject obj = entry.getValue();
-			if(obj.inTransport()) continue;
+        int id = entry.getKey();
+        DrawObject obj = entry.getValue();
+        if(obj.inTransport()) continue;
 
-            if (id == dragID) {
-                drawDragged(g2, debug, obj);
-            } else {
-                if (Math.abs(debug.getX() - obj.getDrawX() - 0.5) < 0.5
-                        && Math.abs(debug.getY() - obj.getDrawY() - 0.5) < 0.5) {
-                    hoverID = id;
-                    hoverLoc = obj.getLocation();
-                    controlBits = obj.getControlBits();
-                }
-                obj.draw(g2, id == focusID || id == hoverID);
-            }
+        if (id == dragID) {
+          drawDragged(g2, debug, obj);
+        } else {
+          if (Math.abs(debug.getX() - obj.getDrawX() - 0.5) < 0.5
+              && Math.abs(debug.getY() - obj.getDrawY() - 0.5) < 0.5) {
+            hoverID = id;
+            hoverLoc = obj.getLocation();
+            controlBits = obj.getControlBits();
+          }
+          obj.draw(g2, id == focusID || id == hoverID);
         }
+      }
         
-        AffineTransform pushed = g2.getTransform();
-        for (Team t : new Team[]{Team.A, Team.B})
+      AffineTransform pushed = g2.getTransform();
+      for (Team t : new Team[]{Team.A, Team.B})
+      {
+        g2.setTransform(pushed);
+        int research = (int)(1.000001*getResearchProgress(t, Upgrade.NUKE.ordinal())*Upgrade.NUKE.numRounds);
+        if (research > 350 && research<370)
         {
-        	g2.setTransform(pushed);
-        	int research = (int)(1.000001*getResearchProgress(t, Upgrade.NUKE.ordinal())*Upgrade.NUKE.numRounds);
-        	if (research > 350 && research<370)
-        	{
-        		DrawObject o = getHQ(t);
-            	g2.translate(o.getDrawX(), o.getDrawY());
-            	g2.translate(0.0, -(research-350)*0.45);
-            	g2.translate(0.0, -1);
-            	BufferedImage bi = t==Team.A ? rNuker.image : rNukeb.image;
-            	AffineTransform trans = AffineTransform.getScaleInstance((1.0 / bi.getWidth()) * o.getRelativeSize(), (1.0 / bi.getWidth()) * o.getRelativeSize());
-            	g2.drawImage(bi, trans, null);
-        	} else if (research > 375)
-        	{
-        		DrawObject o = getHQ(t.opponent());
-            	g2.translate(o.getDrawX(), o.getDrawY());
-            	g2.translate(0.0, -(404-research)*0.45);
-            	g2.translate(0.0, -1);
-            	BufferedImage bi = t==Team.A ? rNuker2.image : rNukeb2.image;
-            	AffineTransform trans = AffineTransform.getScaleInstance((1.0 / bi.getWidth()) * o.getRelativeSize(), (1.0 / bi.getWidth()) * o.getRelativeSize());
-            	g2.drawImage(bi, trans, null);
-            	if (research == 404)
-            	{
-            		double scale = 6.0;
-            		BufferedImage ei = rexplode.image;
-            		trans = AffineTransform.getScaleInstance((scale / ei.getWidth()) * o.getRelativeSize(), (scale / ei.getWidth()) * o.getRelativeSize());
-            		g2.translate(-(scale-1.0)/2, -(scale-1.0)/2-2);
-            		g2.drawImage(ei, trans, null);
-            	}
-        	}
-        	
-        	
+          DrawObject o = getHQ(t);
+          g2.translate(o.getDrawX(), o.getDrawY());
+          g2.translate(0.0, -(research-350)*0.45);
+          g2.translate(0.0, -1);
+          BufferedImage bi = t==Team.A ? rNuker.image : rNukeb.image;
+          AffineTransform trans = AffineTransform.getScaleInstance((1.0 / bi.getWidth()) * o.getRelativeSize(), (1.0 / bi.getWidth()) * o.getRelativeSize());
+          g2.drawImage(bi, trans, null);
+        } else if (research > 375)
+        {
+          DrawObject o = getHQ(t.opponent());
+          g2.translate(o.getDrawX(), o.getDrawY());
+          g2.translate(0.0, -(404-research)*0.45);
+          g2.translate(0.0, -1);
+          BufferedImage bi = t==Team.A ? rNuker2.image : rNukeb2.image;
+          AffineTransform trans = AffineTransform.getScaleInstance((1.0 / bi.getWidth()) * o.getRelativeSize(), (1.0 / bi.getWidth()) * o.getRelativeSize());
+          g2.drawImage(bi, trans, null);
+          if (research == 404)
+          {
+            double scale = 6.0;
+            BufferedImage ei = rexplode.image;
+            trans = AffineTransform.getScaleInstance((scale / ei.getWidth()) * o.getRelativeSize(), (scale / ei.getWidth()) * o.getRelativeSize());
+            g2.translate(-(scale-1.0)/2, -(scale-1.0)/2-2);
+            g2.drawImage(ei, trans, null);
+          }
         }
+        	
+        	
+      }
         
-        if (!debug.isDragging()) {
-            debug.setTarget(hoverID, hoverLoc, controlBits);
-        }
+      if (!debug.isDragging()) {
+        debug.setTarget(hoverID, hoverLoc, controlBits);
+      }
 
     }
 }
