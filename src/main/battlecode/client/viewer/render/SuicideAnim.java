f8c013865001fab827b707b0a72d2052a3131ea7
Aaron Epstein <aepstein824@gmail.com>
2014-01-25 11:24:15 -0500

    no

---
+++ src/main/battlecode/client/viewer/render/SuicideAnim.java
@@ -0,0 +1,24 @@
+package battlecode.client.viewer.render;
+
+import battlecode.common.*;
+import battlecode.client.util.*;
+
+import java.awt.*;
+import java.awt.geom.*;
+
+public class SuicideAnim extends ExplosionAnim {
+  public void draw(Graphics2D g2){
+    super.draw(g2);
+    if (shouldDraw()) {
+      AffineTransform pushed = g2.getTransform();
+      if (loc != null) {
+        g2.translate(loc.x, loc.y);
+      }
+      g2.translate(-0.5*(size - 1), -0.5*(size - 1));
+      g2.setColor(new Color(1.0f, 0, 1));
+      //g2.fill(new Rectangle2D.Float(i + offset, j + offset, size, size));
+      g2.draw(new Ellipse2D.Float(-.5f, -.5f, 2, 2));
+      g2.setTransform(pushed);
+    }
+  }
+}
