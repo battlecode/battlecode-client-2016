b5376fce644756d4c86a7652f397fd4b8bb859ad
Alex X Chen <axcqwerty@gmail.com>
2016-01-10 18:18:50 -0500

    Fix compile

--- src/main/battlecode/client/viewer/render/GameRenderer.java
+++ src/main/battlecode/client/viewer/render/GameRenderer.java
@@ -371,7 +371,7 @@ public class GameRenderer {
 
         switch (actionCommand) {
             case 'A':
-                RenderConfiguration.toggleSpawnRadii();
+                RenderConfiguration.toggleDetails();
                 break;
             case 'B':
                 RenderConfiguration.toggleBroadcast();
@@ -419,7 +419,7 @@ public class GameRenderer {
                 RenderConfiguration.toggleSupplyTransfers();
                 break;
             case 'U':
-                RenderConfiguration.toggleCows();
+                RenderConfiguration.toggleParts();
                 break;
             case 'V':
                 RenderConfiguration.toggleIndicatorDots();
