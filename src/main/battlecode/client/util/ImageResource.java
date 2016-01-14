323103051c366064f6228463fe302c2afcb8a972
Alex X Chen <axcqwerty@gmail.com>
2016-01-13 19:53:38 -0500

    Resize sprites more intelligently and cache the resized versions

--- src/main/battlecode/client/util/ImageResource.java
+++ src/main/battlecode/client/util/ImageResource.java
@@ -1,32 +1,39 @@
 package battlecode.client.util;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ImageResource<E> {
 
-    private final Map<E, ImageFile> cache;
+    private final Map<E, Map<Dimension, ImageFile>> cache;
+
 
     public ImageResource() {
         cache = new HashMap<>();
     }
 
     public ImageFile getResource(E key, String path) {
-        return getResource(key, path, false);
+        return getResource(key, path, 0, 0);
     }
 
-    public ImageFile getResource(E key, String path, boolean useSpriteSheet) {
-        ImageFile img = cache.get(key);
-        if (img != null) {
+    public ImageFile getResource(E key, String path, int width, int height) {
+        if (cache.get(key) != null) {
+            ImageFile img = cache.get(key).get(new Dimension(width, height));
+            if (img != null) {
+                return img;
+            }
+        }
+        ImageFile img = new ImageFile(path);
+        if (width == 0 || height == 0) {
             return img;
         }
-        if (useSpriteSheet) {
-            img = new SpriteSheetFile(path);
-        } else {
-            img = new ImageFile(path);
+        img.resize(width, height);
+        if (cache.get(key) == null) {
+            cache.put(key, new HashMap<>());
         }
-        cache.put(key, img);
+        cache.get(key).put(new Dimension(width, height), img);
         return img;
     }
 }
-
